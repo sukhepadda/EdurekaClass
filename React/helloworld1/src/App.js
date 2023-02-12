77ff4722167846379f84792a455ccf2c9bbd37a0
sukhepadda <paddasukhwantsingh@gmail.com>
2023-02-12 22:13:38 +0530

    a

---
+++ React/helloworld1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1>
+        <hr/>
+        <h4>This is first program in react</h4>
+        <p>This is paragraph in app.js file availabel in src folder.</p>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
