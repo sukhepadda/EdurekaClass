5e86575da4268ed9c303b2bbade8d4593a1374bd
sukhepadda <paddasukhwantsingh@gmail.com>
2023-02-16 22:18:21 +0530

    added live preview of input box

--- React/helloworld1/src/components/InputOutput.js
+++ React/helloworld1/src/components/InputOutput.js
@@ -3,24 +3,47 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const PassingObject = () => {
-    const [name, setname] = useState("padda");
-    useEffect(() => {
-        if(name.length === 0){
-            setname("name is empty");
-        }
-    }, [name]);
-  
+  const [name, setname] = useState("");
+  const [displayname, setdisplayname] = useState("");
+  useEffect(() => {
+    if (name.length === 0) {
+      setdisplayname("Field is empty pls type something");
+    } else {
+      if (name == "padda") {
+        setdisplayname(`${name} is a Developer in future`);
+      }else if(name == "jay"){
+        setdisplayname(`${name} is a Best Tutor`);
+      }else{
+        setdisplayname("You are typing: " + name);
+      }
+    }
+  }, [name]);
+
+  // clear function
+  function clearEverything() {
+    setname("");
+    displayname("");
+  }
+
   const code = (
     <div>
       <h1>Hello to the InputOutput</h1>
-      <input 
-            type="text"
-            className="form-control"
-            placeholder="Enter anything"
-            onChange={e => setname(e.target.value)}
-            value={name}
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter anything"
+        onChange={(e) => setname(e.target.value)}
+        value={name}
       />
-      <h1>{name}</h1>
+      <h1>{displayname}</h1>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={clearEverything}
+      >
+        Clear
+      </button>
     </div>
   );
   return code;
