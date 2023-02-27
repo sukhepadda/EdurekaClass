dea31b2ffde3db1fccc50ff273c72b7c0c5d5e96
sukhepadda <paddasukhwantsingh@gmail.com>
2023-02-27 23:34:58 +0530

    read and delete done

--- Node+Mongo/nodeMongoCrud/src/server/ExpressServer.js
+++ Node+Mongo/nodeMongoCrud/src/server/ExpressServer.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 const bodyParser = require("body-parser");
 
 //connection string
@@ -9,7 +9,7 @@ const connectionString =
 class ExpressServer {
   constructor(
     hostname = process.env.LOCAL_HOST,
-    port = process.env.DEFAULT_PORT |30
+    port = process.env.DEFAULT_PORT
   ) {
     this.serverName = "Express Server API";
     this.hostname = hostname;
@@ -24,6 +24,7 @@ class ExpressServer {
     this.server.use(bodyParser.json()); //for parsing application/json
     this.server.use(bodyParser.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded
 
+    // simple create
     this.server.get("/createsimple", (req, res) => {
       async function run() {
         let cl = new MongoClient(connectionString);
@@ -45,7 +46,83 @@ class ExpressServer {
       }
       run().catch(console.dir);
     });
+    //create from user's values
+    this.server.post("/createuser", (req, res) => {
+      async function run() {
+        let cl = new MongoClient(connectionString);
 
+        try {
+          await cl.connect();
+          let db = cl.db("testing1");
+          let collection = db.collection("heroes");
+          let hero = {
+            name: req.body.name,
+            realname: req.body.realname,
+          };
+          let result = await collection.insertOne(hero);
+          console.log(result);
+          res.send(result);
+        } catch (err) {
+          console.log("erroe in try to connect mongo", err);
+        }
+      }
+      run().catch(console.dir);
+    });
+    //read all data
+    this.server.get("/readall", (req, res) => {
+      async function run() {
+        let cl = new MongoClient(connectionString);
+
+        try {
+          await cl.connect();
+          let db = cl.db("testing1");
+          let collection = db.collection("heroes");
+          const cursor = collection.find();
+          let items = [];
+
+          await cursor.forEach((item) => {
+            let tempItem = {
+              name: item.name,
+              realname: item.realname,
+            };
+            items.push(tempItem);
+          });
+
+          console.log(items);
+          res.send({
+            msg: "Got a req at /readall",
+            result: items,
+          });
+        } catch (err) {
+          console.log("erroe in try to connect mongo", err);
+        }
+      }
+      run().catch(console.dir);
+    });
+    //delete data with id given by user
+    this.server.delete("/delete", (req, res) => {
+      async function run() {
+        let cl = new MongoClient(connectionString);
+
+        try {
+          await cl.connect();
+          let db = cl.db("testing1");
+          let collection = db.collection("heroes");
+          let id = req.body.id;
+          let result = await collection.deleteOne({_id: new ObjectId(id)});
+          console.log(result);
+          res.send({
+            msg: "delete success",
+            result: result,
+          });
+        } catch (err) {
+          console.log("erroe in try to connect mongo", err);
+        }
+      }
+      run().catch(console.dir);
+    });
+
+    // listening to port
     this.server.listen(this.port, () => {
       console.log(
         `${this.serverName} Started at http://${this.hostname}:${this.port}/`
