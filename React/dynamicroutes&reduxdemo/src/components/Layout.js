779247fdce0aa7670bcfa75aa3357d5ad89350fd
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-08 16:58:38 +0530

    dynamic routes and redux folder created

---
+++ React/dynamicroutes&reduxdemo/src/components/Layout.js
@@ -0,0 +1,43 @@
+import { Outlet, Link } from "react-router-dom";
+import Accordion from 'react-bootstrap/Accordion';
+
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import "./Layout.css";
+
+const Layout = () => (
+  <div>
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>React</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+
+            <Nav.Link>
+              <Link to="/simplelist">Simple List</Link>
+            </Nav.Link>
+
+            <Nav.Link>
+              <Link to="/listwithcomponents">List With Components</Link>
+            </Nav.Link>
+
+            <Nav.Link>
+              <Link to="/listwithaccordion">List With Accordion</Link>
+            </Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Outlet />
+  </div>
+);
+
+export default Layout;
