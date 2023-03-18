b7c6059b7937717c3980752b3abe673159e920f1
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-18 11:46:44 +0530

    done

---
+++ React/dynamicroutes/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import ListWithAccordion from "./components/ListWithAccordion";
+import CatPage from "./components/CatPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listwithaccordion" element={<ListWithAccordion />} />
+          <Route path="/listwithaccordion/:id" element={<CatPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/dynamicroutes/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HOME = () => (
+  //we are using round because there is only 1 return value like this a = (bently) => bently;
+  <div>
+    <h1 className="mt-4">Welcome to The Homepage of THE REACT practice folder...</h1>
+  </div>
+);
+
+export default HOME;
